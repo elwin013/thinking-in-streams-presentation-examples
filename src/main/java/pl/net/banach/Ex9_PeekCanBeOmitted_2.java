be1947c3f8c756627b799b61b1bfb553bc18896c
Kamil Banach <kontakt@elwin013.com>
2025-09-20 10:33:47 +0200

    Format Ex9_PeekCanBeOmitted_2.java

--- src/main/java/pl/net/banach/Ex9_PeekCanBeOmitted_2.java
+++ src/main/java/pl/net/banach/Ex9_PeekCanBeOmitted_2.java
@@ -7,8 +7,12 @@ public class Ex9_PeekCanBeOmitted_2 {
     public static void main(String[] args) {
         var random = new Random();
 
-        var numbers = Stream.generate(() -> random.nextInt(10)).limit(10);
-        var count =  numbers.peek(System.out::println).count();
+        var numbers = Stream.generate(() -> random.nextInt(10))
+                            .limit(10);
+
+        var count =  numbers
+                .peek(System.out::println)
+                .count();
         System.out.println("Count: " + count);
     }
 }
